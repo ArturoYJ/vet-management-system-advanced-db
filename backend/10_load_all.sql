daa0c60c11c04497eef16904f05603a8eec5354a
arturoyion <jayj22022email.com@gmail.com>
2026-04-22 04:29:08 -0600

    refactor: rename 00_schema.sql to schema_corte3.sql and move to root, move 10_load_all.sql to backend

--- backend/10_load_all.sql
+++ backend/10_load_all.sql
@@ -1,4 +1,4 @@
-\i /docker-entrypoint-initdb.d/infra/migrations/00_schema.sql
+\i /schema_corte3.sql
 \i /docker-entrypoint-initdb.d/backend/01_procedures.sql
 \i /docker-entrypoint-initdb.d/backend/02_triggers.sql
 \i /docker-entrypoint-initdb.d/backend/03_views.sql
